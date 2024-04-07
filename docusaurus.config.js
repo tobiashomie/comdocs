b0f1ac4c344896e6f4ba8764059f0f9e6f73476c
Smyja <akposlive59@gmail.com>
2024-04-07 09:19:12 +0100

    fix: update link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   tagline: "Developer Documentation",
   favicon: "img/favicon.png",
   // Set the production url of your site here
-  url: "https://comdocs.netlify.app",
+  url: "https://deploy-preview-5--comdocs.netlify.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
